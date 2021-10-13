3db6e05f44aa352077da88e35796c2b9a47ddabc
Noah Lackstein <noah@fossa.com>
2021-10-13 15:23:45 -0700

    Import Faktory daemon

---
+++ cmd/faktory/daemon.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/contribsys/faktory/cli"
+	"github.com/contribsys/faktory/client"
+	"github.com/contribsys/faktory/util"
+	"github.com/contribsys/faktory/webui"
+)
+
+func logPreamble() {
+	log.SetFlags(0)
+	log.Println(client.Name, client.Version)
+	log.Println(fmt.Sprintf("Copyright © %d Contributed Systems LLC", time.Now().Year()))
+	log.Println("Licensed under the GNU Affero Public License 3.0")
+}
+
+func main() {
+	logPreamble()
+
+	opts := cli.ParseArguments()
+	util.InitLogger(opts.LogLevel)
+	util.Debugf("Options: %v", opts)
+
+	s, stopper, err := cli.BuildServer(&opts)
+	if stopper != nil {
+		defer stopper()
+	}
+
+	if err != nil {
+		util.Error("Unable to create Faktory server", err)
+		return
+	}
+
+	err = s.Boot()
+	if err != nil {
+		util.Error("Unable to boot the command server", err)
+		return
+	}
+
+	s.Register(webui.Subsystem(opts.WebBinding))
+
+	go cli.HandleSignals(s)
+	go func() {
+		_ = s.Run()
+	}()
+
+	<-s.Stopper()
+	s.Stop(nil)
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/fossas/faktory-plugins
+
+go 1.17
+
+require github.com/contribsys/faktory v1.5.5
+
+require github.com/stretchr/testify v1.7.0
+
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-redis/redis v6.15.7+incompatible // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20211013171255-e13a2654a71e // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
