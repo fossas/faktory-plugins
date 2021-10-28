4d9449d2cbabea8582e3f92ca409cebdfe7bd01c
Jeremy Gonzalez <jeremy@fossa.com>
2021-10-28 11:38:23 -0600

    add config for disabling plugin

--- uniq/uniq_test.go
+++ uniq/uniq_test.go
@@ -2,6 +2,7 @@ package uniq
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"strconv"
@@ -15,14 +16,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const enabledUniqConfig = `
+[uniq]
+enabled = true
+`
+
 func TestUniq(t *testing.T) {
 	opts := cli.ParseArguments()
 	util.InitLogger("info")
-
+	configDir := t.TempDir()
+	os.Mkdir(fmt.Sprintf("%s/conf.d", configDir), os.FileMode(0777))
 	dir := "/tmp/system.db"
 	defer os.RemoveAll(dir)
-	opts.ConfigDirectory = "."
+	opts.ConfigDirectory = configDir
 	opts.StorageDirectory = dir
+	confgFile := fmt.Sprintf("%s/conf.d/statsd.toml", configDir)
+	if err := ioutil.WriteFile(confgFile, []byte(enabledUniqConfig), os.FileMode(0444)); err != nil {
+		panic(err)
+	}
 	s, stopper, err := cli.BuildServer(&opts)
 	if stopper != nil {
 		defer stopper()
@@ -47,106 +58,127 @@ func TestUniq(t *testing.T) {
 		}
 	}()
 
-	// this is a worker process so we need to set the global WID before connecting
-	client.RandomProcessWid = strconv.FormatInt(rand.Int63(), 32)
+	t.Run("jobs are uniq", func(t *testing.T) {
+		// this is a worker process so we need to set the global WID before connecting
+		client.RandomProcessWid = strconv.FormatInt(rand.Int63(), 32)
 
-	cl, err := client.Dial(client.DefaultServer(), "123456")
-	if err != nil {
-		panic(err)
-	}
-	defer cl.Close()
-	sig, err := cl.Beat()
-	assert.Equal(t, "", sig)
-	assert.NoError(t, err)
+		cl, err := client.Dial(client.DefaultServer(), "123456")
+		if err != nil {
+			panic(err)
+		}
+		defer cl.Close()
+		sig, err := cl.Beat()
+		assert.Equal(t, "", sig)
+		assert.NoError(t, err)
 
-	// create uniqjob
-	job1 := client.NewJob("UniqJob", 1, "string", 3)
-	job1.SetCustom("unique_until", "start")
-	job1.SetCustom("unique_for", 10)
+		// create uniqjob
+		job1 := client.NewJob("UniqJob", 1, "string", 3)
+		job1.SetCustom("unique_until", "start")
+		job1.SetCustom("unique_for", 10)
 
-	job2 := client.NewJob("UniqJob", 1, "string", 3)
-	job2.SetCustom("unique_until", "success")
-	job2.SetCustom("unique_for", 10)
+		job2 := client.NewJob("UniqJob", 1, "string", 3)
+		job2.SetCustom("unique_until", "success")
+		job2.SetCustom("unique_for", 10)
 
-	job3 := client.NewJob("UniqJob", 1, "string", 3)
-	job3.SetCustom("unique_until", "success")
-	job3.SetCustom("unique_for", 1)
+		job3 := client.NewJob("UniqJob", 1, "string", 3)
+		job3.SetCustom("unique_until", "success")
+		job3.SetCustom("unique_for", 1)
 
-	job4 := client.NewJob("UniqJob", 1, "string", 3)
-	job4.SetCustom("unique_until", "success")
-	job4.SetCustom("unique_for", 1)
+		job4 := client.NewJob("UniqJob", 1, "string", 3)
+		job4.SetCustom("unique_until", "success")
+		job4.SetCustom("unique_for", 1)
 
-	otherUniqJob := client.NewJob("Uniq", 2, "string", 3)
+		otherUniqJob := client.NewJob("Uniq", 2, "string", 3)
 
-	err = cl.Push(job1)
-	assert.Nil(t, err)
-
-	err = cl.Push(job2)
-	assert.EqualError(t, err, "NOTUNIQUE Job has already been queued.")
-
-	queue, err := s.Store().GetQueue("default")
-	if err != nil {
-		panic(err)
-	}
-	assert.Equal(t, queue.Size(), uint64(1))
-
-	// job1 processing
-	processJob(cl, func() {
-		// uniq job can be queued since util is start
-		err = cl.Push(job2)
+		err = cl.Push(job1)
 		assert.Nil(t, err)
-	})
 
-	// job2 processing
-	processJob(cl, func() {
-		err = cl.Push(job3)
+		err = cl.Push(job2)
 		assert.EqualError(t, err, "NOTUNIQUE Job has already been queued.")
+
+		queue, err := s.Store().GetQueue("default")
+		if err != nil {
+			panic(err)
+		}
+		assert.Equal(t, queue.Size(), uint64(1))
+
+		// job1 processing
+		processJob(cl, func() {
+			// uniq job can be queued since util is start
+			err = cl.Push(job2)
+			assert.Nil(t, err)
+		})
+
+		// job2 processing
+		processJob(cl, func() {
+			err = cl.Push(job3)
+			assert.EqualError(t, err, "NOTUNIQUE Job has already been queued.")
+		})
+
+		// job can be pushed since it has been finished
+		err = cl.Push(job3)
+		assert.Nil(t, err)
+
+		// wait 1 second for expiration
+		time.Sleep(2 * time.Second)
+		err = cl.Push(job4)
+		assert.Nil(t, err)
+
+		// push another job with same title but other arguments
+		err = cl.Push(otherUniqJob)
+		assert.Nil(t, err)
+
+		// job3, job4, otherUniqJob
+		assert.Equal(t, queue.Size(), uint64(3))
+
+		invalidJob := client.NewJob("UniqJobTwo", 3, "string", 3)
+		invalidJob.SetCustom("unique_until", "other")
+		invalidJob.SetCustom("unique_for", 100)
+		err = cl.Push(invalidJob)
+		assert.EqualError(t, err, "ERR invalid value for unique_until.")
+
+		invalidJob.SetCustom("unique_until", "start")
+		invalidJob.SetCustom("unique_for", 0)
+		err = cl.Push(invalidJob)
+		assert.EqualError(t, err, "ERR unique_for must be greater than or equal to 1.")
+
+		jobWithOutUntil := client.NewJob("UniqueJobThree", 3, "string", 3)
+		jobWithOutUntil.SetCustom("unique_for", 10)
+
+		jobWithOutUntil2 := client.NewJob("UniqueJobThree", 3, "string", 3)
+		jobWithOutUntil2.SetCustom("unique_for", 10)
+
+		err = cl.Push(jobWithOutUntil)
+		assert.Nil(t, err)
+		err = cl.Push(jobWithOutUntil2)
+		assert.EqualError(t, err, "NOTUNIQUE Job has already been queued.")
+
+		hash, err := cl.Info()
+		if err != nil {
+			handleError(err)
+			return
+		}
+		util.Infof("%v", hash)
 	})
 
-	// job can be pushed since it has been finished
-	err = cl.Push(job3)
-	assert.Nil(t, err)
+	t.Run("disabled config", func(t *testing.T) {
+		system := &UniqSubsystem{}
+		config := make(map[string]interface{})
+		config["enabled"] = false
+		s.Options.GlobalConfig["uniq"] = config
 
-	// wait 1 second for expiration
-	time.Sleep(2 * time.Second)
-	err = cl.Push(job4)
-	assert.Nil(t, err)
+		system.Start(s)
+		assert.False(t, system.Options.Enabled)
+	})
 
-	// push another job with same title but other arguments
-	err = cl.Push(otherUniqJob)
-	assert.Nil(t, err)
+	t.Run("no config", func(t *testing.T) {
+		system := &UniqSubsystem{}
+		delete(s.Options.GlobalConfig, "uniq")
 
-	// job3, job4, otherUniqJob
-	assert.Equal(t, queue.Size(), uint64(3))
+		system.Start(s)
+		assert.False(t, system.Options.Enabled)
+	})
 
-	invalidJob := client.NewJob("UniqJobTwo", 3, "string", 3)
-	invalidJob.SetCustom("unique_until", "other")
-	invalidJob.SetCustom("unique_for", 100)
-	err = cl.Push(invalidJob)
-	assert.EqualError(t, err, "ERR invalid value for unique_until.")
-
-	invalidJob.SetCustom("unique_until", "start")
-	invalidJob.SetCustom("unique_for", 0)
-	err = cl.Push(invalidJob)
-	assert.EqualError(t, err, "ERR unique_for must be greater than or equal to 1.")
-
-	jobWithOutUntil := client.NewJob("UniqueJobThree", 3, "string", 3)
-	jobWithOutUntil.SetCustom("unique_for", 10)
-
-	jobWithOutUntil2 := client.NewJob("UniqueJobThree", 3, "string", 3)
-	jobWithOutUntil2.SetCustom("unique_for", 10)
-
-	err = cl.Push(jobWithOutUntil)
-	assert.Nil(t, err)
-	err = cl.Push(jobWithOutUntil2)
-	assert.EqualError(t, err, "NOTUNIQUE Job has already been queued.")
-
-	hash, err := cl.Info()
-	if err != nil {
-		handleError(err)
-		return
-	}
-	util.Infof("%v", hash)
 	s.Stop(nil)
 }
 
