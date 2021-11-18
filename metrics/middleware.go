628f4f69a57cc8f9124f7f56e51bf3ce9a0c2f5f
Noah Lackstein <noah@fossa.com>
2021-11-17 16:09:02 -0800

    Fix metrics reporting (#9)
    * go mod tidy
    
    * Add debug logging
    
    * Fix summary metrics about queues
    - Change COUNT metrics to GAUGE so that they don't get reset to 0 when statsd flushes
    - Calculate queue latency using the oldest job queues
    - Send a queue latency of zero if there are no jobs queued
    
    * Remove duplicate metrics

--- metrics/middleware.go
+++ metrics/middleware.go
@@ -17,10 +17,6 @@ func (m *MetricsSubsystem) addMiddleware() {
 			}
 		}
 
-		if err := m.StatsDClient().Incr(m.PrefixMetricName("succeeded.count"), tags, 1); err != nil {
-			util.Warnf("unable to submit metric: %v", err)
-		}
-
 		return next()
 	})
 	m.Server.Manager().AddMiddleware("fail", func(next func() error, ctx manager.Context) error {
@@ -28,13 +24,6 @@ func (m *MetricsSubsystem) addMiddleware() {
 
 		if ctx.Reservation() != nil {
 			m.StatsDClient().Timing(m.PrefixMetricName("failed.time"), time.Duration(time.Now().Sub(ctx.Reservation().ReservedAt())), tags, 1)
-		}
-
-		if ctx.Job().Failure.RetryCount >= ctx.Job().Retry {
-			// only count a job as failed on the last retry
-			if err := m.StatsDClient().Incr(m.PrefixMetricName("failed.count"), tags, 1); err != nil {
-				util.Warnf("unable to submit metric: %v", err)
-			}
 		}
 		return next()
 	})
