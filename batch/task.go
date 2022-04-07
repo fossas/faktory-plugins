cb6d4031d82d0f710da23ce1b68a0f9cfbb35782
Jeremy Gonzalez <jeremy@fossa.com>
2022-04-07 10:03:25 -0600

    run task in separate go routine (#18)

--- batch/task.go
+++ batch/task.go
@@ -17,7 +17,7 @@ func (t *removeStaleBatches) Name() string {
 
 // Execute - runs the task to collect metrics
 func (t *removeStaleBatches) Execute() error {
-	t.Subsystem.batchManager.removeStaleBatches()
+	go t.Subsystem.batchManager.removeStaleBatches()
 	return nil
 }
 
